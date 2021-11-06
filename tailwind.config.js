981699052f98eb247feea429b2b7789deb8093fc
Angela Yu <angelayu@amazon.com>
2021-11-06 15:28:25 -0700

    added theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
         "bookmark-blue": "#242A45",
         "bookmark-grey": "#9194A2",
         "bookmark-white": "#f7f7f7",
+        "dish-red": "#E41932",
+        "dish-blue": "#003B77",
+        "dish-gold": "#BA9E6B",
+        "dish-purple": "#46237A",
       },
     },
     fontFamily: {
